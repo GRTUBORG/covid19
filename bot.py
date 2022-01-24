df1f5cc724653cdef40c760b2db47cb04697de4e
Dennis <blinhic850@mail.ru>
2022-01-24 12:51:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,3 +33,11 @@ def start_command(message):
 @bot.message_handler(commands = ['help'])
 def send_help(message):
     bot.reply_to(message, "Привет! Рад, что ты заглянул(а) сюда \n• /start — узнать статистику.")
+
+@bot.message_handler(commands = ['covid']
+def send_statics(message):
+    world_cases = covid1.get_total_confirmed_cases() 
+    recovered = covid.get_total_recovered() 
+    active = covid.get_total_active_cases() 
+    deaths = covid.get_total_deaths()
+    bot.send_message(  , "")
