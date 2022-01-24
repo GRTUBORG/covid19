ddac54009f945e67cb9319c21e96ea56987c7664
Dennis <blinhic850@mail.ru>
2022-01-24 12:26:51 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+import telebot
+import time
+import json
+import os
+import re
+import random
+import requests 
+
+from telebot import types
+from datetime import datetime, date, timedelta 
+
+token = 
+bot = telebot.TeleBot(str(token))
+print('Бот работает!')
